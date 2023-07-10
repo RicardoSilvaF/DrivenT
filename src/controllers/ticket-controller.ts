22ecec6ecab754c99fba3e1a823eae7eea5f547c
RicardoSilvaF <zanphs@gmail.com>
2023-07-09 23:31:39 -0300

    Implementação get user tickets

--- src/controllers/ticket-controller.ts
+++ src/controllers/ticket-controller.ts
@@ -11,3 +11,16 @@ export async function getTicketTypes(req: AuthenticatedRequest, res: Response) {
     return res.sendStatus(httpStatus.NO_CONTENT);
   }
 }
+
+export async function getUserTickets(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+  try {
+    const userTickets = await ticketService.getUserTicketsService(userId);
+    if (!userTickets) {
+      return res.status(httpStatus.NOT_FOUND).send('User does not have any tickets');
+    }
+    return res.status(httpStatus.OK).send(userTickets);
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+  }
+}
--- src/services/ticket-service/index.ts
+++ src/services/ticket-service/index.ts
@@ -1,11 +1,61 @@
 import ticketRepository from '@/repositories/ticket-repository';
+import { prisma } from '@/config';
 
 export async function getTicketTypesService() {
   return await ticketRepository.getTicketTypesRepository();
 }
 
+export async function getUserTicketsService(userId: number) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      Enrollment: {
+        include: {
+          Ticket: {
+            include: {
+              TicketType: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!user) {
+    throw new Error(`User with ID ${userId} not found`);
+  }
+
+  const enrollment = user.Enrollment[0];
+  if (!enrollment || enrollment.Ticket.length === 0) {
+    return null;
+  }
+
+  const ticket = enrollment.Ticket[0];
+
+  return {
+    id: ticket.id,
+    status: ticket.status,
+    ticketTypeId: ticket.ticketTypeId,
+    enrollmentId: ticket.enrollmentId,
+    TicketType: {
+      id: ticket.TicketType.id,
+      name: ticket.TicketType.name,
+      price: ticket.TicketType.price,
+      isRemote: ticket.TicketType.isRemote,
+      includesHotel: ticket.TicketType.includesHotel,
+      createdAt: ticket.TicketType.createdAt,
+      updatedAt: ticket.TicketType.updatedAt,
+    },
+    createdAt: ticket.createdAt,
+    updatedAt: ticket.updatedAt,
+  };
+}
+
 const ticketService = {
   getTicketTypesService,
+  getUserTicketsService,
 };
 
 export default ticketService;
