18adf70bd98742e14b2f40ef2456a5c08eb49eae
RicardoSilvaF <zanphs@gmail.com>
2023-07-10 13:56:11 -0300

    Implementação do post payments process

--- src/repositories/payment-repository/index.ts
+++ src/repositories/payment-repository/index.ts
@@ -1,5 +1,7 @@
+import { TicketStatus } from '@prisma/client';
 import { prisma } from '@/config';
 import { notFoundError } from '@/errors';
+import { CardData } from '@/protocols';
 
 export async function getPaymentsByIdRepository(ticketId: number) {
   const payment = await prisma.payment.findFirst({
@@ -13,8 +15,59 @@ export async function getPaymentsByIdRepository(ticketId: number) {
   return payment;
 }
 
+export async function postPaymentsProcessRepository(ticketId: number, cardData: CardData) {
+  const ticket = await prisma.ticket.findUnique({
+    where: {
+      id: ticketId,
+    },
+    include: {
+      TicketType: {
+        select: {
+          price: true,
+        },
+      },
+      Payment: true,
+    },
+  });
+
+  if (!ticket) {
+    throw notFoundError();
+  }
+
+  const payment = await prisma.payment.create({
+    data: {
+      ticketId: ticket.id,
+      value: ticket.TicketType.price,
+      cardIssuer: cardData.issuer,
+      cardLastDigits: cardData.number.toString().slice(-4),
+    },
+  });
+
+  const updatedTicket = await prisma.ticket.update({
+    where: {
+      id: ticket.id,
+    },
+    data: {
+      status: TicketStatus.PAID,
+    },
+  });
+
+  const response = {
+    id: payment.id,
+    ticketId: updatedTicket.id,
+    value: payment.value,
+    cardIssuer: payment.cardIssuer,
+    cardLastDigits: payment.cardLastDigits,
+    createdAt: payment.createdAt,
+    updatedAt: payment.updatedAt,
+  };
+
+  return response;
+}
+
 const paymentRepository = {
   getPaymentsByIdRepository,
+  postPaymentsProcessRepository,
 };
 
 export default paymentRepository;
